198a1c76e59686eb6af54c3611c9a35c887abd19
Shem <shemzeze@gmail.com>
2020-09-19 22:38:12 -0400

    vid n sound moviepy working!!!

--- 3.1 - mouth on dog/3.3 vid n sound moviepy.py
+++ 3.1 - mouth on dog/3.3 vid n sound moviepy.py
@@ -3,7 +3,13 @@ from moviepy.editor import *
 clip = VideoFileClip("Dog small.mp4")
 clip2 = VideoFileClip("Mouth.mp4")
 w, h = clip.size
-sound_both = CompositeAudioClip([clip.volumex(1.2), clip2])
-sound_both.nchannels = max([1])
-sound_both.write_audiofile("both_sound.mp3",  nchannels=1)
+aclip = clip.audio
+aclip2 = clip2.audio
+aclip.set_duration(aclip2.duration)
+# sound_both = concatenate_audioclips([aclip, aclip2])
+sound_both2 = CompositeAudioClip([aclip, aclip2])
+clip.audio = sound_both2
+clip.write_videofile("new.mp4")
+# sound_both2.nchannels = max([2])
+# sound_both2.write_audiofile("both_sound.mp3")
 # print()
